873b07f80283772b9b75ef33f1b3df9b2e8f54c4
rangga48 <ranggaputra681@gmail.com>
2025-05-16 01:37:56 +0700

    Implementasi Detail Listing

--- src/services/listing.service.ts
+++ src/services/listing.service.ts
@@ -8,7 +8,13 @@ export const listingApi = apiSlice.injectEndpoints({
         method: "GET",
       }),
     }),
+    getDetailListing: builder.query({
+      query: (slug: string) => ({
+        url: `/listing/${slug}`,
+        method: "GET",
+      }),
+    }),
   }),
 });
 
-export const { useGetAllListingQuery } = listingApi;
+export const { useGetAllListingQuery, useGetDetailListingQuery } = listingApi;
