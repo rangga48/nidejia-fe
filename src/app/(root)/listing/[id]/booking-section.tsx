7b5d39a7542f3e659d67bc27aa15906bc89f2dbe
rangga48 <ranggaputra681@gmail.com>
2025-05-17 15:54:17 +0700

    Book Handle

--- src/app/(root)/listing/[id]/booking-section.tsx
+++ src/app/(root)/listing/[id]/booking-section.tsx
@@ -1,21 +1,29 @@
 import { Button } from "@/components/atomics/button";
 import Title from "@/components/atomics/title";
+import { useToast } from "@/components/atomics/use-toast";
 import CardBooking from "@/components/molecules/card/card-booking";
 import { DatePickerDemo } from "@/components/molecules/date-picker";
 import { moneyFormat } from "@/lib/utils";
+import { useCheckAvailabilityMutation } from "@/services/transactions.service";
 import moment from "moment";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React, { useMemo, useState } from "react";
 
 interface BookingSectionProps {
-  id: string;
+  id: number;
+  slug: string;
   price: number;
 }
 
-function BookingSection({ id, price }: BookingSectionProps) {
+function BookingSection({ id, slug, price }: BookingSectionProps) {
   const [startDate, setStartDate] = useState<Date>();
   const [endDate, setEndDate] = useState<Date>();
+
+  const { toast } = useToast();
+  const router = useRouter();
+  const [checkAvailability, { isLoading }] = useCheckAvailabilityMutation();
 
   const booking = useMemo(() => {
     let totalDays = 0,
@@ -30,7 +38,45 @@ function BookingSection({ id, price }: BookingSectionProps) {
     }
 
     return { totalDays, subTotal, tax, grandTotal };
-  }, [startDate, endDate]);
+  }, [startDate, endDate, price]);
+
+  const handlerBook = async () => {
+    // `/listing/${id}/checkout`
+    try {
+      const data = {
+        listing_id: id,
+        start_date: moment(startDate).format("YYYY-MM-DD"),
+        end_date: moment(endDate).format("YYYY-MM-DD"),
+      };
+
+      const res = await checkAvailability(data).unwrap();
+      if (res.success) {
+        router.push(
+          `/listing/${slug}/checkout?start_date=${data.start_date}&end_date=${data.end_date}`
+        );
+      }
+      console.log("🚀 ~ handlerBook ~ res:", res);
+    } catch (error: any) {
+      if (error.status === 401) {
+        toast({
+          title: "Something went wrong",
+          description: "Please login first",
+          variant: "destructive",
+          action: (
+            <Link href={`/sign-in?callbackUrl=${window.location.href}`}>
+              Sign In
+            </Link>
+          ),
+        });
+      } else if (error.status === 404) {
+        toast({
+          title: "Something went wrong",
+          description: error.data.message,
+          variant: "destructive",
+        });
+      }
+    }
+  };
   return (
     <div className="w-full max-w-[360px] xl:max-w-[400px] h-fit space-y-5 bg-white border border-border rounded-[20px] p-[30px] shadow-indicator">
       <h1 className="font-bold text-lg leading-[27px] text-secondary">
@@ -69,11 +115,14 @@ function BookingSection({ id, price }: BookingSectionProps) {
           value={moneyFormat.format(booking.grandTotal)}
         />
       </div>
-      <Link href={`/listing/${id}/checkout`}>
-        <Button variant="default" className="mt-4">
-          Book Now
-        </Button>
-      </Link>
+      <Button
+        variant="default"
+        className="mt-4"
+        onClick={handlerBook}
+        disabled={isLoading}
+      >
+        Book Now
+      </Button>
       <div className="bg-gray-light p-5 rounded-[20px] flex items-center space-x-4">
         <Image src="/icons/medal-star.svg" alt="icon" height={36} width={36} />
         <div>
--- src/app/(root)/listing/[id]/page.tsx
+++ src/app/(root)/listing/[id]/page.tsx
@@ -167,7 +167,11 @@ function Detail({ params }: { params: { id: string } }) {
           <CustomerReviews />
         </div>
         {listing && (
-          <BookingSection id={params?.id} price={listing?.price_per_day} />
+          <BookingSection
+            id={listing?.id}
+            slug={listing?.slug}
+            price={listing?.price_per_day}
+          />
         )}
       </section>
 
